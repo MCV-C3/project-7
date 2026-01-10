fb27ea2e5409d2a135a94c566bbf4694995b6904
Miguel Moral <miguelmoralhernandez@gmail.com>
2026-01-10 17:39:49 +0100

    improve regularizers experiment

--- Week3/run_regularizers.sh
+++ Week3/run_regularizers.sh
@@ -24,37 +24,52 @@ LAMBDAS=(0 1e-5 1e-4 1e-3 1e-2)
 L1_RATIOS=(0.1 0.5 0.9)
 
 for REG in "${REG_TYPES[@]}"; do
-    for L in "${LAMBDAS[@]}"; do
-        if [ "${REG}" = "elastic" ]; then
-            for A in "${L1_RATIOS[@]}"; do
+    if [ "${REG}" = "none" ]; then
+        echo "=========================================="
+        echo "Running experiment: REG_TYPE=none, REG_LAMBDA=0, UNFREEZE_BLOCKS=7"
+        echo "=========================================="
+
+        python main.py \
+            --unfreeze_blocks 7 \
+            --reg_type none \
+            --reg_lambda 0 \
+            --l1_ratio 0.5
+
+        echo "Finished REG_TYPE=none, REG_LAMBDA=0"
+        echo ""
+    else
+        for L in "${LAMBDAS[@]}"; do
+            if [ "${REG}" = "elastic" ]; then
+                for A in "${L1_RATIOS[@]}"; do
+                    echo "=========================================="
+                    echo "Running experiment: REG_TYPE=${REG}, REG_LAMBDA=${L}, L1_RATIO=${A}, UNFREEZE_BLOCKS=7"
+                    echo "=========================================="
+
+                    python main.py \
+                        --unfreeze_blocks 7 \
+                        --reg_type ${REG} \
+                        --reg_lambda ${L} \
+                        --l1_ratio ${A}
+
+                    echo "Finished REG_TYPE=${REG}, REG_LAMBDA=${L}, L1_RATIO=${A}"
+                    echo ""
+                done
+            else
                 echo "=========================================="
-                echo "Running experiment: REG_TYPE=${REG}, REG_LAMBDA=${L}, L1_RATIO=${A}, UNFREEZE_BLOCKS=7"
+                echo "Running experiment: REG_TYPE=${REG}, REG_LAMBDA=${L}, UNFREEZE_BLOCKS=7"
                 echo "=========================================="
 
                 python main.py \
                     --unfreeze_blocks 7 \
                     --reg_type ${REG} \
                     --reg_lambda ${L} \
-                    --l1_ratio ${A}
+                    --l1_ratio 0.5
 
-                echo "Finished REG_TYPE=${REG}, REG_LAMBDA=${L}, L1_RATIO=${A}"
+                echo "Finished REG_TYPE=${REG}, REG_LAMBDA=${L}"
                 echo ""
-            done
-        else
-            echo "=========================================="
-            echo "Running experiment: REG_TYPE=${REG}, REG_LAMBDA=${L}, UNFREEZE_BLOCKS=7"
-            echo "=========================================="
-
-            python main.py \
-                --unfreeze_blocks 7 \
-                --reg_type ${REG} \
-                --reg_lambda ${L} \
-                --l1_ratio 0.5
-
-            echo "Finished REG_TYPE=${REG}, REG_LAMBDA=${L}"
-            echo ""
-        fi
-    done
+            fi
+        done
+    fi
 done
 
 echo "ALL REGULARIZER EXPERIMENTS FINISHED"
