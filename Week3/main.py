859019014373e18729039d58e7f4632e2641d847
Miguel Moral <miguelmoralhernandez@gmail.com>
2026-01-10 17:45:28 +0100

    output payh

--- Week3/main.py
+++ Week3/main.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
     )
 
     DATASET_ROOT = '/data/uabmcv2526/shared/dataset/2425/MIT_small_train_1'
-    OUTPUT_PATH = '/data/uabmcv2526/mcvstudent29/output'
+    OUTPUT_PATH = '/data/uabmcv2526/mcvstudent29/output/regularizers'
     OUTPUT_PATH = os.path.join(
         OUTPUT_PATH,
         exp_name
